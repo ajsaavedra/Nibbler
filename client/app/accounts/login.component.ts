6e450dcedda008b79600eb820cbcd07f43a067f8
Tony Saavedra <ajsaavedrax@gmail.com>
2017-08-02 16:29:34 -0700

    Adding form validator to login and signup pages

--- client/app/accounts/login.component.ts
+++ client/app/accounts/login.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './app/accounts/login.component.html'
 })
 
-export class LoginComponent{}
+export class LoginComponent{
+
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = fb.group({
+            'email': [null, Validators.required],
+            'password': [null, Validators.required]
+        })
+    }
+}
--- client/app/accounts/signup.component.ts
+++ client/app/accounts/signup.component.ts
@@ -1,7 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './app/accounts/signup.component.html'
 })
 
-export class SignupComponent{}
+export class SignupComponent {
+
+    signupForm: FormGroup;
+    fnameRegex = new RegExp(/[A-Z][a-z]+/);
+    lnameRegex = new RegExp(/^([A-Z]'?[a-z]+)(-?[A-Z]'?[a-z]+)?$/);
+    emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/);
+
+    constructor(private fb: FormBuilder) {
+        this.signupForm = fb.group({
+            'fname': [null, Validators.compose([
+                Validators.required,
+                Validators.pattern(this.fnameRegex)])],
+            'lname': [null, Validators.compose([
+                Validators.required,
+                Validators.pattern(this.fnameRegex)])],
+            'email': [null, Validators.compose([
+                Validators.required,
+                Validators.pattern(this.emailRegex)])],
+            'password': [null, Validators.compose([
+                Validators.required,
+                Validators.pattern(this.passwordRegex)])]
+        })
+    }
+}
