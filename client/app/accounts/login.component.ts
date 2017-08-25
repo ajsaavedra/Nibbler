116532e11d725d36e87cfb6640445fdada78da41
Tony Saavedra <ajsaavedrax@gmail.com>
2017-08-24 17:10:22 -0700

    Modified navbar and login page

--- client/app/accounts/login.component.ts
+++ client/app/accounts/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -9,10 +10,14 @@ export class LoginComponent{
 
     loginForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder, private router: Router) {
         this.loginForm = fb.group({
             'email': [null, Validators.required],
             'password': [null, Validators.required]
         })
+    }
+
+    rerouteGuest() {
+        this.router.navigateByUrl('/signup');
     }
 }
