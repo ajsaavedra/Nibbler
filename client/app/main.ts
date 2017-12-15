f212b8d74e4b734004f2e1bea5de0976c017fd4c
Tony Saavedra <ajsaavedrax@gmail.com>
2017-12-14 16:25:46 -0800

    Updating app to use bootstrap sass

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-import '../assets/css/custom.css';
+import '../assets/scss/style.scss';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
