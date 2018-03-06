30a429fdeda6254468efb0a1b023eba2ea57e848
Tony Saavedra <ajsaavedrax@gmail.com>
2018-03-05 16:17:40 -0800

    Questions card gets question category when requested

---
+++ client/app/questions/questions-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'questions-card',
+    templateUrl: './app/questions/questions-card.component.html'
+})
+
+export class QuestionsCardComponent {
+    private questionsIcon = require('../../assets/images/questions.svg');
+}
