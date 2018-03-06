205717f767004e3e650da549bf9bafa1e692ab7d
Tony Saavedra <ajsaavedrax@gmail.com>
2018-03-05 16:13:24 -0800

    Cache service updated, can get questions by categories

--- client/app/services/cache.service.ts
+++ client/app/services/cache.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { QuestionService } from './questions.service';
 import { LocationService } from './locations.service';
+import { AccountsService } from './accounts.service';
 
 @Injectable()
 export class CacheService {
     _data;
 
     constructor(private questionService: QuestionService,
-                private locationService: LocationService) {
+                private locationService: LocationService,
+                private accountsService: AccountsService) {
         this._data = {};
     }
 
@@ -15,11 +17,27 @@ export class CacheService {
         this._data['questions'] = this.questionService.getAllQuestions().shareReplay();
     }
 
+    getQuestionsByPopularity() {
+        this._data['popularity'] = this.questionService.getAllPopularQuestions().shareReplay();
+    }
+
     getQuestionById(id) {
         if (!this._data['question']) {
             this._data['question'] = {};
         }
         this._data['question'][id] = this.questionService.getQuestionById(id).shareReplay();
+    }
+
+    getResolvedQuestions() {
+        this._data['resolved'] = this.questionService.getResolvedQuestions().shareReplay();
+    }
+
+    getLikedPosts(uname) {
+        this._data['liked'] = this.accountsService.getLikedPosts(uname).shareReplay();
+    }
+
+    getUnlikedPosts(uname) {
+        this._data['unliked'] = this.accountsService.getUnlikedPosts(uname).shareReplay();
     }
 
     getLocations() {
